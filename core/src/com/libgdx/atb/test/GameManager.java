4c81efdac377f7237bd8856562be975385eac01f
Tillusphilippus <155558200+Tillusphilippus@users.noreply.github.com>
2024-02-14 17:05:54 +0100

    Bug Fixes
    Fixed some Bugs within the Naming of the main Class

--- core/src/com/libgdx/atb/test/GameManager.java
+++ core/src/com/libgdx/atb/test/GameManager.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Game Manager extends ApplicationAdapter {
+public class GameManager extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
--- desktop/src/com/libgdx/atb/test/DesktopLauncher.java
+++ desktop/src/com/libgdx/atb/test/DesktopLauncher.java
@@ -1,8 +1,9 @@
 package com.libgdx.atb.test;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.libgdx.atb.test.Game Manager;
+import com.libgdx.atb.test.GameManager;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -10,6 +11,6 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("ATB-Test");
-		new Lwjgl3Application(new Game Manager(), config);
+		new Lwjgl3Application(new GameManager(), config);
 	}
 }
